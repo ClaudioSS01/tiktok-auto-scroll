0f6d610c9320414e1a3d7a46b3bc508fe69b1da2
CLAUDIO SANTOS DA SILVA <49885783+ClaudioSS01@users.noreply.github.com>
2023-04-11 12:15:56 -0300

    Create tiktok auto scroll pc .js

---
+++ tiktok auto scroll pc .js
@@ -0,0 +1,3 @@
+setInterval(()=>{
+    document.querySelector("#app > div.tiktok-n3whco-DivBodyContainer.e1irlpdw0 > div:nth-child(4) > div > div.tiktok-1tunefa-DivVideoContainer.e11s2kul26 > button.tiktok-1sltbs0-ButtonBasicButtonContainer-StyledVideoSwitch.e11s2kul14").click();
+},120000)
